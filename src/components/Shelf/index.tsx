ae4b8ef722a0fe6b1df7746496ee4f05d5f2189c
Daniel Liberato <danielrcliberato@gmail.com>
2021-09-12 23:46:07 -0300

    feat: added ShelfItem rating

--- src/components/Shelf/index.tsx
+++ src/components/Shelf/index.tsx
@@ -1,389 +1,102 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, useState } from "react";
 import Carousel from "react-elastic-carousel";
-// import ReactStars from "react-rating-stars-component";
+import { ShelfItem } from "./ShelfItem";
 
 import styles from "./shelf.module.css";
 
 export const Shelf = () => {
 	const breakPoints = [
-		{ width: 1, itemsToShow: 2 },
-		{ width: 500, itemsToShow: 3 },
-		{ width: 768, itemsToShow: 4 },
+		{ width: 300, itemsToShow: 1 },
+		{ width: 486, itemsToShow: 2 },
+		{ width: 728, itemsToShow: 3 },
+		{ width: 960, itemsToShow: 4 },
 		{ width: 1200, itemsToShow: 5 },
 	];
-
-	// const ratingChanged = (newRating: number) => {
-	// 	setRating(newRating);
-	// };
 
 	return (
 		<div className={styles.container}>
 			<section className={styles.section}>
-				<h2 className={`${styles.section__title} title`}>Mais Vendidos</h2>
+				<h2 className={`${styles.section__title} title`}>
+					Mais Vendidos
+				</h2>
 
 				<div className={styles.section__content}>
 					<Carousel
 						isRTL={false}
-						pagination={true}
+						pagination={false}
 						breakPoints={breakPoints}
 					>
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
+						<ShelfItem
+							image={"/assets/shelf/sapato-floater-preto.png"}
+							name={"Sapato Floater Preto"}
+							rating={5}
+							flag={true}
+							listPrice={"299,00"}
+							price={"259,90"}
+							installments={"ou em 9x de R$  28,87"}
+						/>
 
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
+						<ShelfItem
+							image={"/assets/shelf/sapato-social.jpg"}
+							name={"Sapato Floater Preto"}
+							rating={5}
+							flag={true}
+							listPrice={"99,00"}
+							price={"79,90"}
+							installments={"ou em 9x de R$ 39,95"}
+						/>
 
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
+						<ShelfItem
+							image={"/assets/shelf/coturno-preto.webp"}
+							name={"Coturno Preto"}
+							rating={5}
+							flag={true}
+							listPrice={"299,00"}
+							price={"223,19"}
+							installments={"ou em 4x de R$ 60,00"}
+						/>
 
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
+						<ShelfItem
+							image={"/assets/shelf/coturno-em-couro-preto.webp"}
+							name={"Coturno em Couro"}
+							rating={5}
+							flag={true}
+							listPrice={"297,70"}
+							price={"147,70"}
+							installments={"ou em 3x de R$ 49,23"}
+						/>
 
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
+						<ShelfItem
+							image={"/assets/shelf/coturno-adventure-trail.jpg"}
+							name={"Coturno Adventure Trail"}
+							rating={5}
+							flag={true}
+							listPrice={"199,00"}
+							price={"132,90"}
+							installments={"ou em 6x de R$ 22,15"}
+						/>
 
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
+						<ShelfItem
+							image={
+								"/assets/shelf/nike-sportswear-venture-runner.webp"
+							}
+							name={"Nike Venture Runner"}
+							rating={5}
+							flag={false}
+							listPrice={"269,89"}
+							price={"269,89"}
+							installments={"ou em 6x de R$ 44,98"}
+						/>
 
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
-
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
-
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
-
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
-
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
-
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
-
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
-
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
-
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
-
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
-
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
-
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
-
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
-
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
-
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
-
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
-
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
-
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
-
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
-
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
-
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
-
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
-
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
-
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
-
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
-
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
-
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
-
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
-
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
-
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
-
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
-
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
-
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
-
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
-
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
-
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
-
-						<article className={`${styles.product} flexColCenter`}>
-							<img
-								className={styles.product__image}
-								src="/assets/shelf/sapato-floater-preto.png"
-								alt="Sapato Floater"
-							/>
-
-							<img
-								className={styles.product__flag}
-								src="/assets/shelf/flag.png"
-								alt="Off"
-							/>
-
-							<h2 className={styles.product__title}>
-								Sapato Floater Preto
-							</h2>
-
-							<div className={styles.product__rating}>
-								{/* <ReactStars
-									count={5}
-									value={rating || product.stars}
-									onChange={ratingChanged}
-									size={24}
-									isHalf={true}
-									activeColor="#F8475F"
-								/> */}
-							</div>
-
-							<div className={styles.product__listPrice}>
-								<span>
-									de R$ 299,00
-								</span>
-							</div>
-
-							<div className={styles.product__price}>
-								<span>por</span>
-								<span>R$ 259,90</span>
-							</div>
-
-							<div className={styles.product__installments}>
-								ou em 9x de R$ 28,87
-							</div>
-
-							<button
-								onClick={() =>{}}
-								className={`btn-primary add-to-cart`}
-							>
-								Comprar
-							</button>
-						</article>
+						<ShelfItem
+							image={"/assets/shelf/polo-joy-casual.jpg"}
+							name={"Tênis Polo Joy Casual"}
+							rating={5}
+							flag={true}
+							listPrice={"199,98"}
+							price={"79,90"}
+							installments={"ou em 2x de R$ 39,95"}
+						/>
 					</Carousel>
 				</div>
 			</section>
